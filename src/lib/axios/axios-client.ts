99fe14d9bfa10bbc5b16ed17a7100401da566cd7
W. Gustavo Cevallos <gstavocevallos@gmail.com>
2025-02-02 23:17:33 -0500

    Make axio api lib

--- src/lib/axios/axios-client.ts
+++ src/lib/axios/axios-client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { getToken } from '../../components/auth/api/auth'
+import { getToken } from '../../components/auth/api/auth.types'
 
 export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
