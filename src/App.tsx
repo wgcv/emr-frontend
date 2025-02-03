933400b214a45036b57cf6210a02b52fefb7a0b6
W. Gustavo Cevallos <gstavocevallos@gmail.com>
2025-02-02 23:16:59 -0500

    Add route for staff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { LoginForm } from './components/auth/components/LoginForm'
-import { NotFound } from './components/common/NotFound'
-import { ProtectedRoute, ProtectedVeterinaryRoute } from './components/common/ProtectedRoute'
+import { I18nextProvider } from 'react-i18next';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { LoginForm } from './components/auth/LoginForm';
+import { NotFound } from './components/common/NotFound';
+import { ProtectedRoute, ProtectedStaffRoute, ProtectedVeterinaryRoute } from './components/common/ProtectedRoute';
+import CreateClinic from './components/staff/clinic/CreateClinic';
+import CreateClinicOwner from './components/staff/clinic/CreateClinicOwner';
+import ViewClinics from './components/staff/clinic/ViewClinics';
+import Settings from './components/staff/Settings';
+import StaffLayout from './components/staff/StaffLayout';
+import i18n from './i18n/i18n';
 
 function App() {
   return (
+    <I18nextProvider i18n={i18n}>
     <Routes>
       <Route path="/login" element={<LoginForm />} />
       <Route element={<ProtectedRoute />}>
@@ -13,11 +21,21 @@ function App() {
         <Route element={<ProtectedVeterinaryRoute />}>
           <Route path="/clinic/dashboard" element={<div>Dashboard Veterinary</div>} />
         </Route>
-        <Route path="/staff/dashboard" element={<div>Dashboard Staff</div>} />
-      </Route>
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route element={<ProtectedStaffRoute />}>
+            <Route element={<StaffLayout />}>
+              <Route path="/staff/dashboard" element={<div>Dashboard Staff</div>} />
+              <Route path="/staff/settings" element={<Settings />} />
+              <Route path="/staff/clinic/view" element={<ViewClinics />} />
+              <Route path="/staff/clinic/create" element={<CreateClinic />} />
+              <Route path="/staff/clinic/create-owner" element={<CreateClinicOwner />} />
+
+            </Route>
+          </Route>
+        </Route>
+        <Route path="/" element={<div>Home</div>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    </I18nextProvider>
   )
 }
 
